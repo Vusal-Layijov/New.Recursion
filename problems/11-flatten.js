68ab2736fa1902ae547eea2935d905ab45949880
Aaron Tuck <Aarontucknyc@gmail.com>
2022-11-08 19:28:08 -0500

    flatten

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -12,10 +12,19 @@ flatten([1, 2]); // [1, 2]
 flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
-function flatten(arr) {
-  // Your code here
+function flatten(arr, arrOne = []) {
+ if (arr.length === 0) return arrOne;
+ for (let ele of arr) {
+  if (typeof ele === 'object') {
+  flatten(ele, arrOne) }
+ else 
+  arrOne.push(ele)
+ }
+ return arrOne;
+
 }
-  
+console.log(flatten([])); // []
+console.log(flatten([1, 2])); // [1, 2]
+console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
-  
